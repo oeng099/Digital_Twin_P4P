57424f2a78ff32e8247c0613849bb10804116b46
wwon866 <wwon866@aucklanduni.ac.nz>
2023-07-16 14:27:15 +1200

    Implemented ChatGPT

--- Components/ChatGPT.js
+++ Components/ChatGPT.js
@@ -1,12 +1,28 @@
-const { Configuration, OpenAIApi } = require("openai");
 
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config()
 const configuration = new Configuration({
-  organization: "org-fLQk0n26lgx1aff0CRQUteBT",
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createChatCompletion({model: "gpt-3.5-turbo",
-messages: [{"role": "system", "content": "You are a digital assistant responsible for monitoring the temperature and state of a room."}, {role: "user", content: "Hello world"}]})
-
-console.log(completion.data.choices[0].message);
+module.exports = {
+    callChatGPT
+}
+async function callChatGPT(text){
+    try {
+        const chatCompletion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "system", content: "You are a digital assistant responsible for monitoring the temperature and state of a room."},{role: "user", content: text}],
+          });
+          console.log(chatCompletion.data.choices[0].message); 
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+    }
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const chatGPT = require('./Components/ChatGPT');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,3 +10,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+chatGPT.callChatGPT("Hello World")
+
+
