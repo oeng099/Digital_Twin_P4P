856b0c251164de2950ac64218db38a151c5f11a9
oeng099 <oeng099@aucklanduni.ac.nz>
2023-08-23 11:51:44 +1200

    Added Display component to HumditiyPage

--- digital_twin/src/pages/HumidityPage.js
+++ digital_twin/src/pages/HumidityPage.js
@@ -1,10 +1,12 @@
 import "./HumidityPage.css";
+import Display from '../components/navigation/Display'
 
 export default function HumidityPage(){
 
       return(
             <div className="hum-page-background">
                   <h1 className="hum-page-title">Humidity</h1>
+                  <Display modulePage="Humidity" moduleMeasurement="70%" targetMeasurement="45%"/>
             </div>
       )
 }
