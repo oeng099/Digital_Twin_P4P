b396086f43909615a95cbb50691af23359d95ab8
oeng099 <oeng099@aucklanduni.ac.nz>
2023-08-23 00:34:27 +1200

    Added Temperature, Humidity and AirQuality pages for module links

---
+++ digital_twin/src/pages/AirQualityPage.js
@@ -0,0 +1,10 @@
+import "./AirQualityPage.css";
+
+export default function AirQualityPage(){
+
+      return(
+            <div className="aq-page-background">
+                  <h1 className="aq-page-title">Air Quality</h1>
+            </div>
+      )
+}
---
+++ digital_twin/src/pages/HumidityPage.js
@@ -0,0 +1,10 @@
+import "./HumidityPage.css";
+
+export default function HumidityPage(){
+
+      return(
+            <div className="hum-page-background">
+                  <h1 className="hum-page-title">Humidity</h1>
+            </div>
+      )
+}
