d67fb1ed54d374839034f5cc7452b88bf14c0251
oeng099 <oeng099@aucklanduni.ac.nz>
2023-10-03 12:18:37 +1300

    Changed current measures on individual pages to query database

--- digital_twin_final/src/pages/AirQualityPage.js
+++ digital_twin_final/src/pages/AirQualityPage.js
@@ -1,12 +1,31 @@
 import "./AirQualityPage.css";
 import Display from '../components/navigation/Display';
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import firestore from "../components/firebase/firebase";
 
 export default function AirQualityPage(){
 
-      return(
-            <div className="aq-page-background">
-                  <h1 className="aq-page-title">Air Quality</h1>
-                  <Display modulePage="Air Quality" moduleMeasurement="50" targetMeasurement="30" moduleAttribute=""/>
-            </div>
-      )
+const [co2, setco2] = useState([]);
+
+const fetchPost = async () => {
+  
+      await getDocs(collection(firestore,"co2"))
+        .then((querySnapshot)=>{
+          const newData = querySnapshot.docs
+            .map((doc) => ({...doc.data(), id:doc.id}));
+            setco2(Number(newData[0]["co2"]));
+        })
+    }
+   
+useEffect(()=>{
+      fetchPost();
+}, [])
+
+return(
+      <div className="aq-page-background">
+            <h1 className="aq-page-title">Air Quality</h1>
+            <Display modulePage="Air Quality" moduleMeasurement={co2} targetMeasurement="30" moduleAttribute=""/>
+      </div>
+)
 }
